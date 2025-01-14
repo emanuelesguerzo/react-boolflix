5c4432034870605597c0504a9416c8c64e1e1db2
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2025-01-14 11:39:53 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+        
+      
+      
+      
+      </BrowserRouter>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+    return (
+        <div>
+            <h2>Benvenuto su Boolflix!</h2>
+        </div>
+    )
+}
+
+export default HomePage;
